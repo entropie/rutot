cd89b8bdabee350d946a201a970626c8abceec3f
Michael Trommer <mictro@gmail.com>
2007-08-22 05:10:01 +0200

    plugins/google.rb: finetuned, gspell added again

--- plugins/google.rb
+++ plugins/google.rb
@@ -11,23 +11,23 @@ def cl_title(t)
   t.gsub(/<.*>(.*)<.*>/, '\1')
 end
 
-# query = google.search "foo"
-# p cl_title( query.results.first.title )
-
-respond_on(:PRIVMSG, prefix_or_nick(:google, :g), :args => [:String], :arg_req => true) do |h|
-  query = google.search(h.args.join(' '))
-  r = query.results.first
-  h.respond "google: \"%s\":%s (approx: %i results)" % [cl_title(r.title), r.url, query.result_count]
+respond_on(:PRIVMSG, prefix_or_nick(:google, :search, :g), :args => [:Everything], :arg_req => true) do |h|
+  begin
+    query = google.search(a=h.args.join(' '))
+    r = query.results.first
+    h.respond "[google] \"%s\":%s (approx: %i results)" % [cl_title(r.title), r.url, query.result_count]
+  rescue
+    h.respond "[google] \"%s\" — No Matches" % a
+  end
 end
 
-respond_on(:PRIVMSG, prefix_or_nick(:gspell, :gs), :args => [:String], :arg_req => true) do |h|
+respond_on(:PRIVMSG, prefix_or_nick(:gspell), :args => [:Everything], :arg_req => true) do |h|
   cp = google.spell(a=h.args.join(' '))
   if cp
-    h.respond("gspell: %s: %s" % [a, cp])
+    h.respond("[gspell]  „%s“: %s" % [a, cp])
   else
-    h.respond "gspell thinks %s is spelled right." % a
+    h.respond "[gspell]  „%s“ is spelled right." % a
   end
-
 end
 
 
