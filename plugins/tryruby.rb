bebf1064d54d594e58af4ddfda54030e96bae079
Michael Trommer <mictro@gmail.com>
2007-08-28 05:33:24 +0200

    reload if session does not respond

--- plugins/tryruby.rb
+++ plugins/tryruby.rb
@@ -31,8 +31,15 @@ class TryRuby
 end
 
 try = TryRuby.new
-respond_on(:PRIVMSG, :irb, prefix_or_nick(:irb), :args => [:Everything], :arg_req => true) do |h|
+respond_on(:PRIVMSG, :irb, prefix_or_nick(:irb, :eval), :args => [:Everything], :arg_req => true) do |h|
+  try.reload_session if try.req("1") == ""
   h.respond try.req(h.args.join(' '))
+  
+end
+
+respond_on(:PRIVMSG, :irbreset, prefix_or_nick(:irb_reset)) do |h|
+  try.reload_session
+  h.respond(ReplyBox.k)
 end
 
 =begin
