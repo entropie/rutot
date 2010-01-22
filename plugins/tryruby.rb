82590f41080569ef0c920030e83dfa1eeeb9180d
Michael Trommer <mictro@gmail.com>
2010-01-22 21:38:44 +0100

    Tryruby plugin disabled

--- plugins/tryruby.rb
+++ plugins/tryruby.rb
@@ -4,19 +4,20 @@
 # Author:  Michael 'entropie' Trommer <mictro@gmail.com>
 #
 
-require 'open-uri'
-include :tryruby
-try = TryRuby.new
+# require 'open-uri'
+# include :tryruby
 
-respond_on(:PRIVMSG, :irb, prefix_or_nick(:irb, :eval), :args => [:Everything], :arg_req => true) do |h|
-  #try.reload_session if try.eval("1") == ""
-  h.respond try.eval(h.args.join(' '))
-end
+# try = TryRuby.new
 
-respond_on(:PRIVMSG, :irbreset, prefix_or_nick(:irb_reset)) do |h|
-  try.reload_session
-  h.respond(ReplyBox.k)
-end
+# respond_on(:PRIVMSG, :irb, prefix_or_nick(:irb, :eval), :args => [:Everything], :arg_req => true) do |h|
+#   #try.reload_session if try.eval("1") == ""
+#   h.respond try.eval(h.args.join(' '))
+# end
+
+# respond_on(:PRIVMSG, :irbreset, prefix_or_nick(:irb_reset)) do |h|
+#   try.reload_session
+#   h.respond(ReplyBox.k)
+# end
 
 =begin
 Local Variables:
