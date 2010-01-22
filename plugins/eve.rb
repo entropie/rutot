621bc66f0c71625c1fd1fcea61744444e89c037b
Michael Trommer <mictro@gmail.com>
2010-01-22 23:37:26 +0100

    kills added

--- plugins/eve.rb
+++ plugins/eve.rb
@@ -5,17 +5,32 @@
 
 require 'reve'
 
-if File.exist?(file="/Users/mit/.eve_api.rb")
-  require file
-else
-  require File.expand_path("~/.eve_api.rb")
-end
+# if File.exist?(file="/Users/mit/.eve_api.rb")
+#   require file
+# else
+#   require File.expand_path("~/.eve_api.rb")
+# end
 
-api = Reve::API.new(UID, APIK)
+require "/Users/mit/eve.rb"
+
+#api = Reve::API.new(UID, APIK)
 
 respond_on(:PRIVMSG, :tq, prefix_or_nick(:tq)) do |h|
   ss = api.server_status
   h.respond("Serverstatus: #{(ss.open and "up" or "down")}#{ss.open and " with #{ss.players} Players"}")
+end
+
+
+respond_on(:PRIVMSG, :kills, prefix_or_nick(:kills), :args => [:String]) do |h|
+  arg = h.args.to_s.strip
+  arg = "D-GTMI" if arg.empty?
+  kills = get_mapkills_from_solarSystem(arg)
+  begin
+    ret = "%s Kills: Ships: %i  Pods: %i  NPCs: %i" % [arg, kills.ship_kills, kills.pod_kills, kills.faction_kills]
+    h.respond(ret)
+  rescue
+    h.respond("%s Kills: dunno" % arg)
+  end
 end
 
 
